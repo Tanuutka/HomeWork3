59aa1932427262ae8500354e1fc7c10f8dc66c9e
Tanuutka <Tanuutka@yandex.ru>
2022-10-26 21:28:05 +0300

    Решение задач

---
+++ Lesson1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Введите пятизначное  число: ");
+
+int palindrom = Convert.ToInt32 (Console.ReadLine());
+  int i1= Math.Abs(palindrom/10000);
+  int i5= Math.Abs(palindrom%10);
+  int i2 =Math.Abs(palindrom/1000%10);
+  int i4 =Math.Abs(palindrom/10%10);
+
+if (Math.Abs(palindrom) > 99999 || Math.Abs(palindrom) < 10000)
+{
+  Console.Write("Вы ввели не пятизначное число, попробуйте еще раз");
+}
+else
+{
+if (i1==i5 && i2==i4)
+{
+ Console.Write("Число является палиндромом ");
+}
+else 
+{
+  Console.Write("Число не является палиндромом");
+}
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите  координаты первой точки: ");
+int x1 = Convert.ToInt32 (Console.ReadLine());
+int y1 = Convert.ToInt32 (Console.ReadLine());
+int z1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите  координаты второй точки: ");
+int x2 = Convert.ToInt32 (Console.ReadLine()); 
+int y2 = Convert.ToInt32 (Console.ReadLine());
+int z2 = Convert.ToInt32 (Console.ReadLine());
+
+int x = x2 - x1;
+int y = y2 - y1;
+int z = z2 - z1; 
+
+double length = Math.Sqrt(x*x + y*y + z*z);
+Console.WriteLine("Длина равна: " + length);
