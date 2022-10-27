126e5644715c5ac0afe74d9da08709609d1811eb
Tanuutka <Tanuutka@yandex.ru>
2022-10-27 08:10:07 +0300

    добавила метод задача 1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,7 +1,4 @@
-﻿void Zadacha19()
-{
-
-    // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
     Console.Write("Введите пятизначное  число: ");
 
     int number = Convert.ToInt32(Console.ReadLine());
@@ -10,7 +7,7 @@
     int i2 = Math.Abs(number / 1000 % 10);
     int i4 = Math.Abs(number / 10 % 10);
     Palindrom(number, i1, i2, i4, i5);
-}
+
 void Palindrom(int number, int i1, int i2, int i4, int i5)
 {
 
@@ -31,4 +28,3 @@ void Palindrom(int number, int i1, int i2, int i4, int i5)
     }
 }
 
-Zadacha19();
--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,5 +1,4 @@
-﻿void Zadacha19()
-{
+﻿
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
@@ -12,7 +11,7 @@ int x2 = Convert.ToInt32(Console.ReadLine());
 int y2 = Convert.ToInt32(Console.ReadLine());
 int z2 = Convert.ToInt32(Console.ReadLine());
 FindLength(x1, x2, y1, y2, z1, z2);
-}
+
 
 void FindLength(int x1, int x2, int y1, int y2, int z1, int z2)
 {
@@ -24,4 +23,3 @@ double length = Math.Sqrt(x * x + y * y + z * z);
 length = Math.Round(length, 2);
 Console.WriteLine("Длина равна: " + length);
 }
-Zadacha19();
