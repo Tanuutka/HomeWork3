2a721af007a964a147fc230c611205b62cc0a5fc
Tanuutka <Tanuutka@yandex.ru>
2022-10-27 08:04:28 +0300

    добавила метод

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,24 +1,34 @@
-﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-Console.Write("Введите пятизначное  число: ");
+﻿void Zadacha19()
+{
 
-int palindrom = Convert.ToInt32 (Console.ReadLine());
-  int i1= Math.Abs(palindrom/10000);
-  int i5= Math.Abs(palindrom%10);
-  int i2 =Math.Abs(palindrom/1000%10);
-  int i4 =Math.Abs(palindrom/10%10);
+    // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+    Console.Write("Введите пятизначное  число: ");
 
-if (Math.Abs(palindrom) > 99999 || Math.Abs(palindrom) < 10000)
-{
-  Console.Write("Вы ввели не пятизначное число, попробуйте еще раз");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int i1 = Math.Abs(number / 10000);
+    int i5 = Math.Abs(number % 10);
+    int i2 = Math.Abs(number / 1000 % 10);
+    int i4 = Math.Abs(number / 10 % 10);
+    Palindrom(number, i1, i2, i4, i5);
 }
-else
+void Palindrom(int number, int i1, int i2, int i4, int i5)
 {
-if (i1==i5 && i2==i4)
-{
- Console.Write("Число является палиндромом ");
+
+    if (Math.Abs(number) > 99999 || Math.Abs(number) < 10000)
+    {
+        Console.Write("Вы ввели не пятизначное число, попробуйте еще раз");
+    }
+    else
+    {
+        if (i1 == i5 && i2 == i4)
+        {
+            Console.Write("Число является палиндромом ");
+        }
+        else
+        {
+            Console.Write("Число не является палиндромом");
+        }
+    }
 }
-else 
-{
-  Console.Write("Число не является палиндромом");
-}
-}
+
+Zadacha19();
